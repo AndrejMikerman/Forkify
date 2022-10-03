eb4516b86ef04f5241c4e8f2577bf8b5eb8c59c1
AndrejMikerman <111794283+AndrejMikerman@users.noreply.github.com>
2022-10-03 23:12:51 +0200

    added new welcome feature

--- src/js/controller.js
+++ src/js/controller.js
@@ -112,6 +112,10 @@ const controlAddRecipe = async function (newRecipe) {
   }
 };
 
+const newFeature = function () {
+  console.log('Welcome to the application!');
+};
+
 const init = function () {
   bookmarksView.addHandlerRender(controlBookmarks);
   recipeView.addHandlerRender(controlRecipes);
@@ -120,6 +124,7 @@ const init = function () {
   searchView.addHandlerSearch(controlSearchResults);
   paginationView.addHandlerClick(controlPagination);
   addRecipeView.addHandlerUpload(controlAddRecipe);
+  newFeature();
 };
 
 init();
